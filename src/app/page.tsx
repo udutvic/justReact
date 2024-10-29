34c7c55acae1c7d17b89ebbfac0140e28ea70598
udutvic <udutvic@gmail.com>
2024-10-29 22:40:47 +0100

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 // import Header from "../app/components/Header";
 import Sidebar from "../app/components/Sidebar";
 // import CodeEditor from "../app/components/CodeEditor";
-import CodeSandbox from "../app/components/CodeSandbox";
+// import CodeSandbox from "../app/components/CodeSandbox";
 
 export default function App() {
   return (
